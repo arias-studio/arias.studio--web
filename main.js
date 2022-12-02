c1b33dbd52f5ea1bd0abf02c887a829bd132617d
arias.studio <arias.ccb@gmail.com>
2022-12-02 15:42:04 -0500

    7.1
    Se corrigieron errores

--- main.js
+++ main.js
@@ -3,11 +3,11 @@ window.addEventListener('load', () => {
 	quicklink.listen();
 });
 /* ------- Memorize Scroll Position Across Page Loads ------- */
-let sidebar = document.querySelector(".sidebar");
-let top = localStorage.getItem("sidebar-scroll");
-if (top !== null) {
-	sidebar.scrollTop = parseInt(top, 10);
-}
-window.addEventListener("beforeunload", () => {
-	localStorage.setItem("sidebar-scroll", sidebar.scrollTop);
-});
+// let sidebar = document.querySelector(".sidebar");
+// let top = localStorage.getItem("sidebar-scroll");
+// if (top !== null) {
+// 	sidebar.scrollTop = parseInt(top, 10);
+// }
+// window.addEventListener("beforeunload", () => {
+// 	localStorage.setItem("sidebar-scroll", sidebar.scrollTop);
+// });
