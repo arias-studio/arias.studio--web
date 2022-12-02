e5e6f3388a823f3adc8ad99a69323793ab468aa0
arias.studio <arias.ccb@gmail.com>
2022-12-02 12:28:22 -0500

    v7
    Sin barba.js. Se agrgó QuickLink

--- main.js
+++ main.js
@@ -1,2 +1,13 @@
-barba.init({
+/* --------------------- QuickLink Init --------------------- */
+window.addEventListener('load', () => {
+	quicklink.listen();
+});
+/* ------- Memorize Scroll Position Across Page Loads ------- */
+let sidebar = document.querySelector(".sidebar");
+let top = localStorage.getItem("sidebar-scroll");
+if (top !== null) {
+	sidebar.scrollTop = parseInt(top, 10);
+}
+window.addEventListener("beforeunload", () => {
+	localStorage.setItem("sidebar-scroll", sidebar.scrollTop);
 });
