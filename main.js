f25be7b63a22f661a19f162bf9a270842b18b851
arias.studio <arias.ccb@gmail.com>
2022-12-23 11:15:48 -0500

    10.0
    Versión corregido el scroll del sidebar y se puso indicador de sección activa

--- main.js
+++ main.js
@@ -1,3 +1,28 @@
-window.addEventListener('load', () => {
+window.addEventListener("DOMContentLoaded", () => {
+
+	// Memorize Scroll Position Across Page Loads
+	let sidebar = document.querySelector(".sidebar");
+	let top = sessionStorage.getItem("sidebar-scroll");
+	if (top !== null) {
+		sidebar.scrollTop = parseInt(top, 10);
+	}
+	window.addEventListener("beforeunload", () => {
+		sessionStorage.setItem("sidebar-scroll", sidebar.scrollTop);
+	});
+
+	// Initialize Quicklink
 	quicklink.listen();
+
+	// Mark Active Page in Navigation
+	const activePage = window.location.pathname;
+	if (activePage !== "") {
+		const navLinks = document.querySelectorAll('nav a').forEach(link => {
+			if (link.href === `${activePage}` || link.href === `${window.location.origin}${activePage}`) {
+				link.classList.add('active');
+				console.log(activePage);
+				console.log(link.href);
+			}
+		});
+	}
+
 });
